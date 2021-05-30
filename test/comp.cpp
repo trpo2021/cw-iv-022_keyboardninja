34daba5796c84fa947c41643c226d1ebaaacb4e2
Selvvias <nowell.creative.nc@gmail.com>
2021-05-30 15:56:19 +0700

    Add test structure.

---
+++ test/comp.cpp
@@ -0,0 +1,42 @@
+//Сравнение полученных от пользователя данных с исходником
+#include "../src/keynlibs/mainlib.hpp"
+
+int scompare(char *cstring, char * userstring, uint8_t lang, int *fails) {
+
+    int j = 0;
+    bool flag = 1;
+
+    while (cstring[j] != '\0') {
+        if (cstring[j] != userstring[j]) {
+            if (lang == ENG) {
+                (*fails)++;
+            }
+            else if (lang == RU ) {
+                flag = 0;
+                (*fails)++;
+                break;
+            }
+            flag = 0;
+        }
+        else {
+            if (lang == ENG) {
+                if (cstring[j + 1] == '\0') {
+                    if (userstring[j + 1] == '\0') {
+                        break;
+                    }
+                    else {
+                        flag = 0;
+                    }
+                } 
+            }
+            else if (lang == RU && cstring[j + 1] == '\0' && userstring[j + 1] == '\0') {
+            }
+        }
+        j++;
+    }
+
+    if (flag == 0)
+        return 0;
+
+    return 1;
+}
