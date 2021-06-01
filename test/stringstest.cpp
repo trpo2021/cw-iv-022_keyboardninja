ca66a0bebb87e26825590752145c84aeac288c4e
Selvvias <nowell.creative.nc@gmail.com>
2021-06-01 12:47:22 +0700

    Add string function tests.

---
+++ test/stringstest.cpp
@@ -0,0 +1,21 @@
+#include "../src/keynlibs/mainlib.hpp"
+
+int slen(char* str)
+{
+    int len;
+
+    for (int i = 0; str[i] != '\0'; i++)
+        len = i + 1;
+
+    return len;
+}
+
+int lencount(char** str, int count)
+{
+    int length = 0;
+
+    for (int i = 0; i < count; i++) {
+        length = length + slen(str[i]);
+    }
+    return length;
+}
